689a29a5c14793975af6b83d97ce89612ceb4231
grihabor <grihabor@mail.ru>
2017-09-25 14:27:11 +0300

    analyze code

--- task_1/2017_420_borodin.c
+++ task_1/2017_420_borodin.c
@@ -1,52 +1,66 @@
+
+/*
+This is modified version of task code. Use it to analyze program behaviour
+
 int h;
 void
 f (int a, int b)
 {
-  int x, y;
-  x = 10;
-  y = 5;
-  h = 3;
-  x = 4;
-  y = 9;
-  if (h < y - x)
-    {
-      if (y < 7)
-        {
-          if (h > 5)
-            {
-              y = 6;
+    int x, y;
+1:  x = 10;
+2:  y = 5;
+3:  h = 3;
+4:  x = 4;
+5:  y = 9;
+
+    always true
+    if (h < y - x) {
+
+        // always false
+        if (y < 7) {
+            if (h > 5) {
+                y = 6;
             }
-          x = 1;
+            x = 1;
         }
     }
-}
+6:}
 
 void
-g (int a, int b)
-{
-  int x, y;
-  x = 4;
-  y = 3;
-  h = b;
-  if (y > 5)
-    {
-      x = 10;
+g (int a, int b) {
+
+    int x, y;
+1:  x = 4;
+2:  y = 3;
+3:  h = b;
+
+    always false
+    if (y > 5) {
+         x = 10;
+    } else {
+4:       h = x; // h = 4;
     }
-  else
-    {
-      h = x;
-    }
-  x = 3;
-  y = 3;
-  while (x < 2)
-    {
-      if (h > 0)
-        break;
-      if (h < x + y)
-        {
-          h = y - x;
-          y = 3;
-          x = 9;
+5:  x = 3;
+6:  y = 3;
+
+    always false
+    while (x < 2) {
+        if (h > 0) {
+            break;
+        }
+        if (h < x + y) {
+            h = y - x;
+            y = 3;
+            x = 9;
         }
     }
+
+7:}
+*/
+
+
+
+int main(int argc, char** argv) {
+    
 }
+
